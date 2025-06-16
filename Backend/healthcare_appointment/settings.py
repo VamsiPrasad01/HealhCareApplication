4949ddb9e79a4bfdde73b2e7943ca243999f3270
VamsiPrasad01 <vamsiprasad571@gmail.com>
2025-06-16 09:43:11 +0200

    s

--- Backend/healthcare_appointment/settings.py
+++ Backend/healthcare_appointment/settings.py
@@ -46,11 +46,10 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',      
+    'corsheaders.middleware.CorsMiddleware',  # ✅ MUST be first
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -134,7 +133,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # 👈 React dev server
-]
-
+CORS_ALLOW_ALL_ORIGINS = True  # 🚨 Use only in development!
